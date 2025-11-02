95dc04e731502591bbffa380e0814b3212d47f41
monica-ss1 <143162362+monica-ss1@users.noreply.github.com>
2025-11-01 23:58:03 -0700

    P2recv.cpp

--- P2recv.cpp
+++ P2recv.cpp
@@ -14,24 +14,21 @@
 using namespace std;
 
 int main() {
-    // Set message queue attributes
     struct mq_attr attr;
-    attr.mq_flags = 0;          // blocking queue
-    attr.mq_maxmsg = 10;        // max 10 messages
-    attr.mq_msgsize = MAX_SIZE; // max message size
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = MAX_SIZE;
     attr.mq_curmsgs = 0;
 
-    // Create the message queue (or open if exists)
     mqd_t mq = mq_open(QUEUE_NAME, O_CREAT | O_RDONLY, 0644, &attr);
     if (mq == (mqd_t)-1) {
         perror("mq_open");
         exit(1);
     }
 
-    char buffer[MAX_SIZE + 1]; // buffer for received message
+    char buffer[MAX_SIZE + 1];
     ssize_t bytes_read;
 
-    // Open file to write received data
     int out_fd = open("file_recv", O_CREAT | O_WRONLY | O_TRUNC, 0644);
     if (out_fd == -1) {
         perror("open file_recv");
@@ -43,19 +40,16 @@ int main() {
     while (true) {
         memset(buffer, 0, sizeof(buffer));
 
-        // Receive message (blocking)
         bytes_read = mq_receive(mq, buffer, MAX_SIZE, nullptr);
         if (bytes_read < 0) {
             perror("mq_receive");
             break;
         }
 
-        // Check for "empty" message (end of file)
         if (bytes_read == 0 || strncmp(buffer, MSG_STOP, 4) == 0) {
             break;
         }
 
-        // Write received data to file
         if (write(out_fd, buffer, bytes_read) == -1) {
             perror("write");
             break;
@@ -64,7 +58,8 @@ int main() {
 
     close(out_fd);
     mq_close(mq);
-    mq_unlink(QUEUE_NAME); // delete the message queue
+    mq_unlink(QUEUE_NAME);
 
     return 0;
 }
+
